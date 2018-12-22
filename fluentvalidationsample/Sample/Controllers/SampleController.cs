64218c92dc38645a65c815fbf694d9bb42956e95
Andrea Tosato <andrea.tosato@hotmail.it>
2018-12-22 21:33:42 +0100

    Add Validator custom in Controller

--- fluentvalidationsample/Sample/Controllers/SampleController.cs
+++ fluentvalidationsample/Sample/Controllers/SampleController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Sample.ViewModels;
 
@@ -13,6 +10,16 @@ namespace Sample.Controllers
     {
         [HttpPost("Person")]
         public IActionResult Post([FromBody] PersonViewModel value)
+        {
+            if (ModelState.IsValid)
+                return Ok();
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost("PersonRule")]
+        public IActionResult PostRule(
+            [CustomizeValidator(RuleSet = Validators.PersonValidator.ValidaNumeriTelefono)]
+            [FromBody] PersonViewModel value)
         {
             if (ModelState.IsValid)
                 return Ok();
